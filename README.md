e4ef6df67b08ac3926b44a2f9289722d0c0b1dfb
3went2 <webb.png@gmail.com>
2017-07-06 19:34:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# claypot-mongoose-plugin
+
+**THIS LIBRARY IS UNDER DEVELOPING, PLEASE DON'T USE IT IN PRODUCTION ENV**
+
+## Installing
+
+```bash
+$ yarn add claypot claypot-mongoose-plugin
+```
+
+**DOCUMENT IS COMING SOON**
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,53 @@
+
+import mongoose from 'mongoose';
+import mongooseStore from 'cache-manager-mongoose';
+import { logger } from 'claypot';
+import join from 'path';
+import importModules from 'import-modules';
+
+const { connection } = mongoose;
+
+export default class MongooseClaypotPlugin {
+	constructor(options = {}, { root }) {
+		this._name = options.name || 'mongoose';
+		this._schemasDir = join(root, options.schemas || 'schemas');
+	}
+
+	connectDB(register) {
+		register(
+			this._name,
+			({
+				host = '127.0.0.1',
+				port = 27017,
+				database,
+				...other,
+			}) => {
+				mongoose.connect(`mongodb://${host}:${port}/${database}`, other);
+
+				connection.on('error', ({ message }) => {
+					logger.fatal('[MONGODB CONNECTION ERROR]:', message);
+				});
+
+				connection.once('open', () => logger.info('mongodb connected.'));
+
+			},
+			mongooseStore,
+		);
+	}
+
+	registerModels(register, names) {
+		const schemas = importModules(this._schemasDir);
+		register(
+			this._name,
+			names.reduce((models, name) => {
+				const schema = schemas[name];
+				if (schema) {
+					const modelName = (name + '').toLowerCase();
+					const model = mongoose.model(modelName, schema || schema.default);
+					models[name] = model;
+				}
+				return models;
+			}, {}),
+		);
+	}
+}
