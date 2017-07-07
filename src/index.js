150e13ff5b38a615657c5732221f8e0c5a31b15e
3went2 <webb.png@gmail.com>
2017-07-07 19:04:58 +0800

    make compatible with claypot v0.0.0-beta.31

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 import mongoose from 'mongoose';
-// import mongooseStore from 'cache-manager-mongoose';
+import mongooseStore from 'cache-manager-mongoose';
 import { logger } from 'claypot';
 import { join } from 'path';
 import importModules from 'import-modules';
@@ -11,44 +11,50 @@ mongoose.Promise = Promise;
 export default class MongooseClaypotPlugin {
 	constructor(options = {}, { root }) {
 		this._name = options.name || 'mongoose';
-		this._schemasDir = join(root, options.schemas || 'schemas');
+		const schemaDir = join(root, options.schemas || 'schemas');
+		this._schemas = importModules(schemaDir);
 	}
 
-	connectDB(register) {
-		register(
-			this._name,
-			({
-				host = '127.0.0.1',
-				port = 27017,
-				database,
-				...other,
-			}) => {
-				mongoose.connect(`mongodb://${host}:${port}/${database}`, other);
+	registerDatabase(register) {
 
+		register(this._name, {
+
+			connect(options) {
+				const {
+					host = '127.0.0.1',
+					port = 27017,
+					database,
+					...other,
+				} = options;
+
+				mongoose.connect(`mongodb://${host}:${port}/${database}`, other);
 				connection.on('error', ({ message }) => {
 					logger.fatal('[MONGODB CONNECTION ERROR]:', message);
 				});
-
 				connection.once('open', () => logger.info('mongodb connected.'));
-
 			},
-			// mongooseStore,
-		);
+
+			createCache(options) {
+				return {
+					...options,
+					store: mongooseStore,
+					mongoose,
+				};
+			},
+
+			createModels: (names) => {
+				return names.reduce((models, name) => {
+					const schema = this._schemas[name];
+					if (schema) {
+						const modelName = (name + '').toLowerCase();
+						const model = mongoose.model(modelName, schema.default || schema);
+						models[name] = model;
+					}
+					return models;
+				}, {});
+			},
+
+		});
 	}
 
-	registerModels(register, names) {
-		const schemas = importModules(this._schemasDir);
-		register(
-			this._name,
-			names.reduce((models, name) => {
-				const schema = schemas[name];
-				if (schema) {
-					const modelName = (name + '').toLowerCase();
-					const model = mongoose.model(modelName, schema.default || schema);
-					models[name] = model;
-				}
-				return models;
-			}, {}),
-		);
-	}
 }
